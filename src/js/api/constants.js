bd6a39e531aa4f195cde230e14d05f9df8822a60
TonjeSchjefstad <tonsch03841@stud.noroff.no>
2025-11-19 13:41:38 +0100

    Adds Register Functionality

---
+++ src/js/api/constants.js
@@ -0,0 +1,17 @@
+export const API_KEY = "db7225e1-9983-4e59-a55d-6f2cb506417d";
+
+export const API_BASE = "https://v2.api.noroff.dev";
+
+export const API_AUTH = `${API_BASE}/auth`;
+
+export const API_AUTH_LOGIN = `${API_AUTH}/login`;
+
+export const API_AUTH_REGISTER = `${API_AUTH}/register`;
+
+export const API_AUTH_KEY = `${API_AUTH}/create-api-key`;
+
+export const API_AUCTION = `${API_BASE}/auction`;
+
+export const API_LISTINGS = `${API_AUCTION}/listings`;
+
+export const API_PROFILES = `${API_AUCTION}/profiles`;
