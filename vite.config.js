6c4186b02d7119802e1dd362d77c857634678b8f
TonjeSchjefstad <tonsch03841@stud.noroff.no>
2025-12-03 14:02:27 +0100

    fix: add multi-page configuration for Vite build

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,20 @@ export default defineConfig({
   build: {
     outDir: "dist",
     sourcemap: true,
+    rollupOptions: {
+      input: {
+        main: "./index.html",
+        listings: "./listings/index.html",
+        listing: "./listings/listing/index.html",
+        createListing: "./listings/create/index.html",
+        editListing: "./listings/edit/index.html",
+        profile: "./profile/index.html",
+        editProfile: "./profile/edit/index.html",
+        about: "./about/index.html",
+        contact: "./contact/index.html",
+        faq: "./faq/index.html",
+        notFound: "./404.html",
+      },
+    },
   },
 });
